a01fe7fb6531452fee306e8403037fddbb1e4dfd
fulara1102 <fularagaurav1102@gmail.com>
2024-01-29 00:12:34 +0530

    program updated

--- searchingAlgoritm/src/com/fulara/BinarySearch/bs6.java
+++ searchingAlgoritm/src/com/fulara/BinarySearch/bs6.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class bs6 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] arr = new int[5];
-        for (int u = 0; u < arr.length;u++){
-            arr[u] = in.nextInt();
+        System.out.println("Enter the size of Array : ");
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the element of array : ");
+        for (int i = 0; i < arr.length;i++){
+            arr[i] = in.nextInt();
         }
+        System.out.println("Enter the target : ");
         int tar = in.nextInt();
         int res = solve(arr,tar);
-        System.out.println(res);
+        System.out.println("The element is at "+res+" index position");
     }
     public static int solve(int[] a, int target){
         int start = 0;
@@ -25,25 +29,29 @@ public class bs6 {
                 return mid;
             }
             if (isAsc){
+                int flag = 0;
                 if (target > a[mid]){
                     start = mid + 1;
                 }
                 else {
                     end = mid - 1;
                 }
-                if (isDes){
-                    if (target < a[mid]){
+                flag = 1;
+            }
+            if (isDes){
+                int flag1 = 0;
+                if (target < a[mid]){
                         start = mid + 1;
                     }
                     else {
                         end = mid - 1;
                     }
+                    flag1 = 1;
                 }
-                else {
-                    return -1;
-                }
+//            else {
+//                    return -1;
+//                }
             }
-        }
         return -1;
+        }
     }
-}
