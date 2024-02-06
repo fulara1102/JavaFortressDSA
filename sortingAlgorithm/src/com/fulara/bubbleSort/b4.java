5bbbea368df46b87f5e5b82f143403ff4beeb45b
fulara1102 <fularagaurav1102@gmail.com>
2024-02-07 00:14:01 +0530

    added : bubble sort

---
+++ sortingAlgorithm/src/com/fulara/bubbleSort/b4.java
@@ -0,0 +1,36 @@
+package com.fulara.bubbleSort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class b4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] a = new int[5];
+        System.out.println("Enter the array : ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = in.nextInt();
+        }
+        bubbleSort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void bubbleSort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int flag = 0;
+            for (int j = 0; j < a.length - 1 - i; j++) {
+                flag = 0;
+                if (a[j] > a[j + 1]) {
+                    //swap
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    flag = 1;
+                }
+            }
+            if (flag == 0) {
+                break;
+            }
+        }
+    }
+}
